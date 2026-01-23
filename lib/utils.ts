f24bfa06a04be7e3419d5e1774f4136090e60b42
ShootyQ <andrewpcarlson85@gmail.com>
2026-01-23 11:07:48 -0600

    Fix getAssetPath implementation for GitHub Pages

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 export const getAssetPath = (path: string) => {
-    const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+    const isProd = process.env.NODE_ENV === 'production';
+    const basePath = isProd ? '/SpaceAdventure' : '';
     const cleanPath = path.startsWith('/') ? path : `/${path}`;
     return `${basePath}${cleanPath}`;
 };
