91e03761723d2e46db72b20087372661f72c378f
ShootyQ <andrewpcarlson85@gmail.com>
2026-02-05 19:55:15 -0600

    deploy fix

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,6 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
-                    </div>
                 </div>
 
             </div>
