c3e5e4eee269b49f9bee68ad8e6f2ee97ef571ec
ShootyQ <andrewpcarlson85@gmail.com>
2026-02-06 21:03:47 -0600

    Deploy fix, I doubt it.

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -5,7 +5,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "mock-project-id",
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "mock-client-email@example.com",
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQMC\n-----END PRIVATE KEY-----",
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCd+t5A3s+w\n2M+dJ/2zJ4u5O9/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\ny6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/y6/\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCd+t5A3s+w\n-----END PRIVATE KEY-----",
     }),
   });
 }
