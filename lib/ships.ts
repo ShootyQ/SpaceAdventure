0d7d5077a9271e475062a8b8b149542600f1cc9a
ShootyQ <andrewpcarlson85@gmail.com>
2026-02-19 12:27:46 -0600

    new names for ships

--- lib/ships.ts
+++ lib/ships.ts
@@ -14,20 +14,20 @@ export const SHIP_OPTIONS: ShipOption[] = [
     { id: "coconutship", name: "Tropical Drifter", type: "cruiser", assetPath: "/images/ships/coconutship.png" },
     { id: "dragoneggship", name: "Dragon Scale Pod", type: "scout", assetPath: "/images/ships/dragoneggship.png" },
 
-    { id: "earth-xp-earn", name: "Earth XP Earn", type: "cargo", assetPath: "/images/collectibles/ships/xp-unlocks/earth-xp earn.png" },
-    { id: "mars-xp-earn", name: "Mars XP Earn", type: "cargo", assetPath: "/images/collectibles/ships/xp-unlocks/mars-xp earn.png" },
-    { id: "mercury-xp-earn", name: "Mercury XP Earn", type: "cargo", assetPath: "/images/collectibles/ships/xp-unlocks/mercury-xp earn.png" },
-    { id: "venus-xp-earn", name: "Venus XP Earn", type: "cargo", assetPath: "/images/collectibles/ships/xp-unlocks/venus-xp earn.png" },
+    { id: "earth-xp-earn", name: "Terra Vanguard", type: "cargo", assetPath: "/images/collectibles/ships/xp-unlocks/earth-xp earn.png" },
+    { id: "mars-xp-earn", name: "Crimson Striker", type: "cargo", assetPath: "/images/collectibles/ships/xp-unlocks/mars-xp earn.png" },
+    { id: "mercury-xp-earn", name: "Solar Needle", type: "cargo", assetPath: "/images/collectibles/ships/xp-unlocks/mercury-xp earn.png" },
+    { id: "venus-xp-earn", name: "Aurora Lance", type: "cargo", assetPath: "/images/collectibles/ships/xp-unlocks/venus-xp earn.png" },
 
-    { id: "earth-uncommon", name: "Earth Uncommon", type: "scout", assetPath: "/images/collectibles/ships/uncommon/earth-uncommon.png" },
-    { id: "mars-uncommon", name: "Mars Uncommon", type: "scout", assetPath: "/images/collectibles/ships/uncommon/mars-uncommon.png" },
-    { id: "mercury-uncommon", name: "Mercury Uncommon", type: "scout", assetPath: "/images/collectibles/ships/uncommon/mercury-uncommon.png" },
-    { id: "venus-uncommon", name: "Venus Uncommon", type: "scout", assetPath: "/images/collectibles/ships/uncommon/venus-uncommon.png" },
+    { id: "earth-uncommon", name: "Blue Meridian", type: "scout", assetPath: "/images/collectibles/ships/uncommon/earth-uncommon.png" },
+    { id: "mars-uncommon", name: "Dustrunner Mk II", type: "scout", assetPath: "/images/collectibles/ships/uncommon/mars-uncommon.png" },
+    { id: "mercury-uncommon", name: "Helios Dart", type: "scout", assetPath: "/images/collectibles/ships/uncommon/mercury-uncommon.png" },
+    { id: "venus-uncommon", name: "Cloudpiercer", type: "scout", assetPath: "/images/collectibles/ships/uncommon/venus-uncommon.png" },
 
-    { id: "earth-extremely-rare", name: "Earth Extremely Rare", type: "cruiser", assetPath: "/images/collectibles/ships/extremely-rare/earth-extremely rare.png" },
-    { id: "mars-extremely-rare", name: "Mars Extremely Rare", type: "cruiser", assetPath: "/images/collectibles/ships/extremely-rare/mars-extremelyrare.png" },
-    { id: "mercury-extremely-rare", name: "Mercury Extremely Rare", type: "cruiser", assetPath: "/images/collectibles/ships/extremely-rare/mercury-extremelyrare.png" },
-    { id: "venus-extremely-rare", name: "Venus Extremely Rare", type: "cruiser", assetPath: "/images/collectibles/ships/extremely-rare/venus-extremelyrare.png" },
+    { id: "earth-extremely-rare", name: "Gaia Sovereign", type: "cruiser", assetPath: "/images/collectibles/ships/extremely-rare/earth-extremely rare.png" },
+    { id: "mars-extremely-rare", name: "Red Dreadnought", type: "cruiser", assetPath: "/images/collectibles/ships/extremely-rare/mars-extremelyrare.png" },
+    { id: "mercury-extremely-rare", name: "Perihelion Wraith", type: "cruiser", assetPath: "/images/collectibles/ships/extremely-rare/mercury-extremelyrare.png" },
+    { id: "venus-extremely-rare", name: "Celestial Siren", type: "cruiser", assetPath: "/images/collectibles/ships/extremely-rare/venus-extremelyrare.png" },
 ];
 
 const SHIP_ASSET_PATHS = new Map<string, string>(
